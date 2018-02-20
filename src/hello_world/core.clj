4f5eabbeb3ecbd8ce9ee12e642b8346a9c7baa1b
Camilo Olarte <juan.c.olarte@gmail.com>
2018-02-20 14:25:07 -0500

    port 3000 for test

--- src/hello_world/core.clj
+++ src/hello_world/core.clj
@@ -1,12 +1,16 @@
 (ns hello-world.core
   (:require [compojure.core :refer :all]
-            [org.httpkit.server :refer [run-server]])
+            [org.httpkit.server :refer [run-server]]
+             [clojure.java.shell :as sh])
+
   (:gen-class))
 
 (defroutes myapp
-  (GET "/" [] "Hola esto es la nueva version branch carajo"))
+  (GET "/" [] "Hola esto es la nueva version branch carajo")
+  (GET "/env" [] (:out (sh/sh "env"))))
+
 
 (defn -main []
-  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
     (run-server myapp {:port port})
     (println (str "Listening on port " port))))
